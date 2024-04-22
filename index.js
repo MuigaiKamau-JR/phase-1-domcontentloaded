22f947605ae9426a38bd23ef628f38861cba6205
Kamau <kamauwamuigaijr@gmail.com>
2024-04-22 12:41:02 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// My code
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+  
+  function updateDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";// changes the text dosplayed
+  }
